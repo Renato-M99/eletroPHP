7a152232de27ca1fcab2618a4d94c3c7ea177690
Rnt <renatojunior7@hotmail.com>
2024-05-22 00:12:05 -0300

    notebooks

--- notebook.php
+++ notebook.php
@@ -7,38 +7,38 @@
     </li>
     <li class="item">
         <img src="./img/notebook2.png" alt="">
-        <h3>title</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <h3>Notebook Basic</h3>
+        <p>AMD Ryzen 3, 8GB DDR4 de RAM, 240 SSD, Windows 11</p>
     </li>
     <li class="item">
         <img src="./img/notebook3.png" alt="">
-        <h3>title</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <h3>Notebook Modern</h3>
+        <p>Intel Core i3, 12GB de RAM DDR4, 512GB SSD, Ubuntu</p>
     </li>
     <li class="item">
         <img src="./img/notebook4.png" alt="">
-        <h3>title</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <h3>Notebook Modern 2.0</h3>
+        <p>Intel Core i5, 12GB de RAM DDR4, 512GB SSD, Windows 11 ou Ubuntu(linux)</p>
     </li>
     <li class="item">
         <img src="./img/notebook5.png" alt="">
-        <h3>title</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <h3>Notebook Gamer de entrada</h3>
+        <p>AMD Ryzen 5, 16GB de RAM DDR4, 512GB SSD, RTX 3060 8GB GDDR5, Windows 11</p>
     </li>
     <li class="item">
         <img src="./img/notebook6.png" alt="">
-        <h3>title</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <h3>Notebook Gamer Mid-Level</h3>
+        <p>AMD Ryzen 5, 16GB de RAM DDR4, 1TB SSD, RTX 3060 12GB GDDR6, Windows 11</p>
     </li>
     <li class="item">
         <img src="./img/notebook7.png" alt="">
-        <h3>title</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <h3>Notebook AlienWare</h3>
+        <p>Intel Core i7, 32 GB de RAM DDR4, 1TB SSD, RTX 4070ti 16GB GDDR6, Windows 11</p>
     </li>
     <li class="item">
         <img src="./img/notebook8.png" alt="">
-        <h3>title</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <h3>Notebook AMD</h3>
+        <p>AMD Ryzen 7, 16GB de RAM DDR4, 1TB SSD, AMD Radeo, RX Série 7000M, Windows 11</p>
     </li>
 </ul>
 <link rel="stylesheet" href="./styles/layout.css">
